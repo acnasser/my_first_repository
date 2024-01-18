d03c5f0767c176b7883aeedd828754337bbccf27
acnasser-uncw <acnasser@uncw.edu>
2024-01-18 17:04:10 +0000

    next code

--- scripts/code.py
+++ scripts/code.py
@@ -1,2 +1,9 @@
-# adding just one line to this code 
-print ("I just added my first line here")
+import random
+import itertools
+
+letters = "difhqwf"
+perms = list(itertools.permutations(letters))
+random.shuffle(perms)
+
+for i in range(1000):
+    print("".join(perms[i]))
